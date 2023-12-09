4c7b9b83854d7f106b7a178b1383a0377998c481
Jean <>
2023-12-09 13:06:47 +0100

    update querySelector for cover-image and artists

--- src/content.js
+++ src/content.js
@@ -25,7 +25,6 @@ function setup(attempts) {
   }
 
   function getArtistElement() {
-    //return document.querySelector('[data-testid="context-item-info-subtitles"] span > a[data-testid="context-item-info-artist"][href^="/artist"]');
     return Array.from(document.querySelector('[data-testid="context-item-info-subtitles"]').querySelectorAll('a[data-testid="context-item-info-artist"]')).map(artistLink => artistLink.textContent.trim());
   }
 
@@ -36,11 +35,6 @@ function setup(attempts) {
   function getSong() {
     return getSongElement().innerText;
   }
-
-    /*
-  function getArtist() {
-    return getArtistElement().innerText;
-  }*/
 
   async function saveNowPlaying() {
     const blob = new Blob([JSON.stringify(data, null, 2)], { type: "application/json" });
